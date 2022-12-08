9ab21471bb4a5c33707960899640c343b128eb24
Wborg <wilicious@gmail.com>
2022-12-08 17:20:20 -0300

    stack navigation example

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Routes } from "./src/routes";
+// import { ScreenA } from "./src/screens/ScreenA";
+// import { ScreenB } from "./src/screens/ScreenB";
+
+export default function App() {
+  return (
+    // <ScreenA />
+    <Routes />
+  );
+}
+
